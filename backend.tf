abadba0486ec6d65c3a153ec773233b0373633ea
kristiyan krastev <rumenovkristiyan@gmail.com>
2025-09-18 18:17:37 +0000

    nimoa

--- backend.tf
+++ backend.tf
@@ -4,6 +4,5 @@ terraform {
     storage_account_name = "kristiyansterraform"
     container_name       = "private-app"
     key                  = "terraform.private-app"
-    use_azuread_auth     = true
   }
 }
